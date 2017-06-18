7d6127313a7662e645938e395f0e10d0ef8e5ebf
Brian Picciano <mediocregopher@gmail.com>
2017-06-17 21:50:08 -0600

    got 'heartbeats' kinda working

--- src/viz/core.cljs
+++ src/viz/core.cljs
@@ -12,7 +12,10 @@
 (defn- debug [& args]
   (.log js/console (clojure.string/join " " (map str args))))
 
+(defn- positive [n] (if (> 0 n) (- n) n))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; initialization
 
 (defn- window-partial [k]
   (int (* (aget js/document "documentElement" k) 0.95)))
@@ -25,14 +28,23 @@
 (defn- new-state []
   {:frame-rate 15
    :exit-wait-frames 40
-   :tail-length 15
+   :tail-length 7
    :frame 0
+   :dial 0
    :gif-seconds 0
    :grid-width 35 ; from the center
    :ghost (-> (ghost/new-ghost grid/isometric)
               (ghost/new-active-node [0 0])
               )
    })
+
+(defn setup []
+  (let [state (new-state)]
+    (q/frame-rate (:frame-rate state))
+    state))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; scaling and unit conversion related
 
 (defn- curr-second [state]
   (float (/ (:frame state) (:frame-rate state))))
@@ -42,15 +54,40 @@
                   (float (/ (:grid-width state) (window-size 0)))))]
              [(:grid-width state) h]))
 
-(defn- positive [n] (if (> 0 n) (- n) n))
+(defn- scale [state xy]
+  (map-indexed #(* %2 (float (/ (window-half-size %1)
+                                ((grid-size state) %1)))) xy))
 
-(defn- spawn-chance [state]
-  "Gives a chance of spawning based on the current run time"
-  (let [period-seconds 1
-        period-frames (* (:frame-rate state) period-seconds)]
-  (if (zero? (rem (:frame state) period-frames))
-    1 100)
-    ))
+(def bounds-buffer 1)
+
+(defn- in-bounds? [state pos]
+  (let [[w h] (apply vector (map #(- % bounds-buffer) (grid-size state)))
+        min-bound [(- w) (- h)]
+        max-bound [w h]
+        pos-k (keep-indexed #(let [mini (min-bound %1)
+                                   maxi (max-bound %1)]
+                               (when (and (>= %2 mini) (<= %2 maxi)) %2)) pos)]
+    (= (count pos) (count pos-k))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; dials
+
+(def dial-period-seconds 2)
+(def dial-offset-seconds 0.5)
+(def dial-width-frames 1)
+
+(defn- set-dial [state]
+  (let [period-frames (* (:frame-rate state) dial-period-seconds)
+        frame-in-second (rem (:frame state) (int period-frames))
+        offset-frame (int (* (:frame-rate state) dial-offset-seconds))]
+    (assoc state :dial
+           (if (or (and (>= frame-in-second 0) (< frame-in-second dial-width-frames))
+                   (and (>= frame-in-second offset-frame) (< frame-in-second (+ dial-width-frames offset-frame))))
+             1
+             0))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; poss-fn
 
   ;(let [period-seconds 1
   ;      rad-per-second (float (/ (/ Math/PI 2) period-seconds))
@@ -60,53 +97,28 @@
   ;    (if (> chance-raw 0.97) 3 50)
   ;  ))
 
-(defn- spawn-all? [state]
-  (let [period-seconds 3
-        period-frames (* (:frame-rate state) period-seconds)]
-  (zero? (rem (:frame state) period-frames))))
-
 (defn- dist-from [pos1 pos2]
   (reduce + (map #(* % %) (map - pos1 pos2))))
 
+(def mk-poss-fns {
+                  :random (fn [state]
+                            (fn [pos adj-poss]
+                              (take 2 (random-sample 0.6 adj-poss))))
+
+                  :centered (fn [state]
+                              (let [spawn-all? (= 1 (:dial state))]
+                                (fn [pos adj-poss]
+                                  (let [to-take (if spawn-all? (count adj-poss) 1)]
+                                    (take to-take (sort-by #(dist-from % [0 0]) adj-poss))
+                                    ))))
+               })
+
 (defn- mk-poss-fn [state]
-  (let [spawn-all? (spawn-all? state)]
-    (fn [pos adj-poss]
-      (let [adj-poss (filter #(in-bounds? state %) adj-poss)
-            to-take (if spawn-all? (count adj-poss) 1)]
-        (take to-take (sort-by #(dist-from % [0 0]) adj-poss))
-        ))))
+  (let [chosen-poss-fn :centered
+        inner-fn ((chosen-poss-fn mk-poss-fns) state)]
+    (fn [pos adj-poss] (inner-fn pos (filter #(in-bounds? state %) adj-poss)))))
 
-;(defn- mk-poss-fn [state]
-;  (let [chance (spawn-chance state)]
-;    (fn [pos adj-poss]
-;      (if (zero? (rand-int chance))
-;        adj-poss
-;        (take 1 (shuffle adj-poss))))
-;    ))
-
-;(defn- mk-poss-fn [state]
-;  (fn [pos adj-poss]
-;    (take 2 (random-sample 0.6 adj-poss))))
-
-(defn setup []
-  (let [state (new-state)]
-    (q/frame-rate (:frame-rate state))
-    state))
-
-(defn- scale [state xy]
-  (map-indexed #(* %2 (float (/ (window-half-size %1)
-                                ((grid-size state) %1)))) xy))
-
-(defn- quil-bounds [state buffer]
-  (let [[w h] (apply vector (map #(- % buffer) (grid-size state)))]
-    [[(- w) (- h)] [w h]]))
-
-(defn- in-bounds? [state pos]
-  (let [[min-bound max-bound] (quil-bounds state 1)
-        pos-k (keep-indexed #(let [mini (min-bound %1)
-                                   maxi (max-bound %1)]
-                               (when (and (>= %2 mini) (<= %2 maxi)) %2)) pos)]
-    (= (count pos) (count pos-k))))
+;; ghost
 
 (defn- ghost-incr [state poss-fn]
   (update-in state [:ghost] ghost/incr poss-fn))
@@ -118,9 +130,13 @@
 (defn- ghost-update-node-meta [state id f]
   (update-in state [:ghost :forest] forest/update-node-meta id f))
 
-(defn- ghost-set-nodes-red [state]
-  (reduce #(ghost-update-node-meta %1 %2 (fn [ma] (assoc m :color 0xFFFF0000)))
-          state (get-in state [:ghost :active-node-ids])))
+(defn- ghost-set-nodes-color [state]
+  (let [color (if (zero? (:dial state)) 0xFF000000 0xFFFF0000)]
+    (reduce #(ghost-update-node-meta %1 %2 (fn [ma] (assoc m :color color)))
+            state (get-in state [:ghost :active-node-ids]))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; update
 
 (defn- maybe-exit [state]
   (if (empty? (get-in state [:ghost :active-node-ids]))
@@ -131,11 +147,15 @@
 (defn update-state [state]
   (let [poss-fn (mk-poss-fn state)]
     (-> state
+        (set-dial)
         (ghost-incr poss-fn)
-        (ghost-set-nodes-red)
+        (ghost-set-nodes-color)
         (maybe-remove-roots)
         (update-in [:frame] inc)
         (maybe-exit))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; draw
 
 (defn- draw-ellipse [state pos size] ; size is [w h]
   (let [scaled-pos (scale state pos)
@@ -220,6 +240,9 @@
     ;            (gstring/format "spawn-chance:%d" (spawn-chance state))))
     ;        30 30)
   )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; def
 
 (q/defsketch viz
   :title ""
