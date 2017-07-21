da3f361a08412fbbf8cbdde14859433fa45f0af3
Brian Picciano <mediocregopher@gmail.com>
2017-07-20 20:06:51 -0600

    project.clj changes, presumably to get repl working

--- project.clj
+++ project.clj
@@ -3,12 +3,22 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [quil "2.6.0"]
-                 [gil "1.0.0-SNAPSHOT"]
-                 [org.clojure/clojurescript "1.9.473"]]
+                 ;[gil "1.0.0-SNAPSHOT"]
+                 [org.clojure/clojurescript "1.9.473"]
+                 ;[lein-cljsbuild "1.1.6"]
+                 ]
 
-  :plugins [[lein-cljsbuild "1.1.5"]]
+  :plugins [[lein-cljsbuild "1.1.5"]
+            ]
+
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.2"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   }}
+
   :hooks [leiningen.cljsbuild]
 
   :cljsbuild
@@ -17,5 +27,12 @@
              {:output-to "js/main.js"
               :output-dir "out"
               :main "viz.core"
+              ;:externs ["externs/processing.js"]
+              :preamble ["processing.min.js"]
+              ;:optimizations :advanced
               :optimizations :none
-              :pretty-print true}}]})
+              :pretty-print false}}
+            ]
+   }
+
+  )
